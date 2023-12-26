6f8d3ba6a838a0fce569114a6712e1bb23631f3e
Hiiro007 <nguyenthaithanh07@gmail.com>
2023-12-26 11:31:34 +0700

    Update story and add subheadline

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,12 @@ module.exports = {
       },
 
       colors: {
-        'light-coffee': '#C89F94'
+        'coffee': {
+          50: '#E8D6D0',
+          200: '#C89F94',
+          400: '#A25F4B',
+          600: '#744838'
+        }
       },
 
       keyframes: {
